644daed5b279f536526063497e2cfdec61c6f50f
Mahmudul Hasan <ctw00696@criticaltechworks.com>
2020-12-13 14:12:05 +0000

    final rock scissor paper game

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -8,6 +8,18 @@ def play():
         userInput = askUser()
     computer = random.choice(['r', 's', 'p'])
     
+    if(userInput == computer):
+        print("You and computer has the same entry play again!")
+        play()
+    
+    if isWin(userInput, computer):
+        print('Yay! you have won the game!')
+    else:
+        print('Sorry, you have lost the game :(')
+def isWin(userInput, computer):
+    if (userInput=='r' and computer=='s') or (userInput == 'p' and computer== 'r') or(userInput=='s' and computer=='p'):
+        return True
+    return False
 def askUser():
     user = input("Enter your option between 'r', 's', 'p' >>>>>>").lower()
     return user
